3b52c61947c02952f2a0dd20a567ad523bca0ff2
Brian Heise <brian.heise@woven-planet.global>
2025-01-30 22:42:15 +0900

    adds toggle button

--- ui/components/sort_button/src/lib.rs
+++ ui/components/sort_button/src/lib.rs
@@ -1,11 +1,25 @@
-use leptos::{component, view, IntoView};
+use leptos::{
+    component,
+    prelude::{RwSignal, Update},
+    view, IntoView,
+};
 use thaw::{Button, ButtonSize};
 
 #[component]
 pub fn sort_button() -> impl IntoView {
+    let icon = RwSignal::new(icondata::BsSortUp);
+
+    let on_click = move |_| {
+        icon.update(|icon| {
+            *icon = if *icon == icondata::BsSortUp {
+                icondata::BsSortDown
+            } else {
+                icondata::BsSortUp
+            }
+        });
+    };
+
     view! {
-        <Button
-            icon=icondata::BsSortUp
-            size=ButtonSize::Small />
+        <Button icon on_click size=ButtonSize::Small />
     }
 }
