e9650de93842fd1d36d4f974467ccad4accdbc30
Brian Heise <brian.heise@woven-planet.global>
2025-02-11 15:38:08 +0900

    dedicated not found path

--- ui/src/app.rs
+++ ui/src/app.rs
@@ -1,4 +1,7 @@
-use crate::{data_source::DataSource, views::table::MountainTable};
+use crate::{
+    data_source::DataSource,
+    views::{not_found::NotFound, table::MountainTable},
+};
 use leptos::{component, view, IntoView};
 use leptos_router::{
     components::{Outlet, ParentRoute, Route, Router, Routes},
@@ -11,10 +14,11 @@ pub fn App(data_source: impl DataSource) -> impl IntoView {
     view! {
         <ConfigProvider>
             <Router>
-                <Routes fallback=|| "Not found.">
+                <Routes fallback=NotFound>
                     <ParentRoute path=path!("/:lang") view=Outlet>
                         <Route path=path!("/lists/:list_name/table") view=move || view! { <MountainTable data_source /> }/>
                     </ParentRoute>
+                    <Route path=path!("/not-found") view=NotFound />
                 </Routes>
             </Router>
         </ConfigProvider>
